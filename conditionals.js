c6fc8b05b214ee5f5fef29c737e74ca13053f073
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-10-31 01:32:00 +0000

    Use conditionals to tell people the weather

---
+++ conditionals.js
@@ -0,0 +1,32 @@
+/*const raining = true;
+const cold = false;
+
+if (raining){
+  console.log("Don't forget your umbrella!");
+}
+if (cold){
+  console.log("Make sure you puck out a scarf!");
+}
+
+console.log("Now you're ready to go outside!")
+
+const cold = false;
+
+if (cold){
+  console.log("Make sure you puck out a scarf!");
+} else {
+  console.log("Short sleeves are fine.")
+}
+*/
+
+const temp = 15;
+
+if (temp < 0){
+  console.log("Make sure you pick out a scarf!");
+} else if (temp < 15){
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
