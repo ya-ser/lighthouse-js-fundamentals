ad8d5fe31832f2585f1e0d054e1f907160fd6eeb
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-11-04 23:14:11 +0000

    created a while/for loop that prints ingredients and also made a for loop that does it in reverse order

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+/*
+console.log("Banana Bread Ingredients:");
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+console.log("Banana Bread Ingredients:");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+*/
+console.log("Banana Bread Ingredients:");
+for (let i = ingredients.length -1; i>= 0; i--) {
+  console.log(ingredients[i]);
+}
